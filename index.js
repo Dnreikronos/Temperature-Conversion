a9cc7ee54d2c195b46eedc471d31d41f35d65688
João Roberto Soares <dnreikronos2@gmail.com>
2024-07-21 11:18:36 -0300

    Adjusted the CSS ant create the calculums for the converter

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 
+const input = document.getElementById("input");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
 
-function converter() {
-    
+function converte() {
+    if(toFahrenheit.checked){
+        temp = Number(input.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F"
+    }
+    else if(toCelsius.checked){
+        temp = Number(input.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C"
+    }
+    else{
+        result.textContent= "Select a unit";
+    }
 }
